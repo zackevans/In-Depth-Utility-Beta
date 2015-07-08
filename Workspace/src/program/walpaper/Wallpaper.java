6351db463e543a306a82dabae78e2e07017feb48
Zack Evans <ZackEVans@Zacks-MacBook-Pro.local>
2015-07-08 03:08:21 -0400

    added wallpaper

---
+++ Workspace/src/program/walpaper/Wallpaper.java
@@ -0,0 +1,36 @@
+package program.walpaper;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+public class Wallpaper extends JPanel
+{
+	private Image img;
+
+	public Wallpaper(String img) 
+	{
+		this(new ImageIcon(img).getImage());
+	}
+
+	public Wallpaper(Image img) 
+	{
+		this.img = img;
+		Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+		setPreferredSize(size);
+		setMinimumSize(size);
+		setMaximumSize(size);
+		setSize(size);
+		setLayout(null);
+	}
+
+	public void paintComponent(Graphics g) 
+	{
+		g.drawImage(img, 0, 0, null);
+	}
+
+}
