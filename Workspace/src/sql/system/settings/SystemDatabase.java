4e1c38cc24ae02b559175e61e20bd77d63cb866a
Zack Evans <zackalta15@gmail.com>
2015-07-10 19:56:53 -0400

    added check row method

--- Workspace/src/sql/system/settings/SystemDatabase.java
+++ Workspace/src/sql/system/settings/SystemDatabase.java
@@ -2,6 +2,7 @@ package sql.system.settings;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class SystemDatabase 
@@ -36,6 +37,46 @@ public class SystemDatabase
 	    
 	    System.out.println("System Settings Table created successfully");
 	}
+	
+	public static boolean checkRow()
+	{
+		Connection c = null;
+	    Statement stmt = null;
+	    boolean rVal = false;
+	    
+	    try 
+	    {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:IDU_Files/IDU_User.db");
+	      c.setAutoCommit(false);
+	     
+
+	      stmt = c.createStatement();
+	      ResultSet rs = stmt.executeQuery("SELECT * FROM SYSTEM_SETTINGS WHERE ID = 1");
+	      while (rs.next()) 
+	      {
+	    	  int i = rs.getInt("ID");
+	   
+	    	  if (i == 1)
+	    	  {
+	    		  rVal = true;
+	    	  }
+	      }
+	      
+	      rs.close();
+	      stmt.close();
+	      c.close();
+	    } 
+	    catch ( Exception e ) 
+	    {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	    
+	    return rVal;
+	}
+	
+	
 	
 	
 	
