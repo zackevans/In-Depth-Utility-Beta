e9015b857e76d6d07862821e717c4b5bf1180909
Zack Evans <zackalta15@gmail.com>
2015-07-24 10:58:18 -0400

    added components for remove pass

---
+++ Workspace/src/menu/settings/security/removepass/EnterPassField.java
@@ -0,0 +1,30 @@
+package menu.settings.security.removepass;
+
+import java.awt.Graphics;
+
+import javax.swing.JPasswordField;
+
+import menu.buffer.BufferPanel;
+
+public class EnterPassField extends JPasswordField
+{
+	BufferPanel bufferPanel;
+	
+	public EnterPassField (BufferPanel bufferPanel) 
+    {
+        this.bufferPanel = bufferPanel;
+    }
+
+ 	protected void paintComponent(Graphics g) 
+    {
+         g.setColor(getBackground());
+         g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+         super.paintComponent(g);
+    }
+    
+    protected void paintBorder(Graphics g) 
+    {
+         g.setColor(getForeground());
+         g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15,15);
+    }
+}
