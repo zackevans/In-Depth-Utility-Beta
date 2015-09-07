77fbc5a5d0ad4be4d4c5f044ba832b5d19514b10
Zack Evans <zackalta15@gmail.com>
2015-09-06 21:34:40 -0400

    added option pane

--- Workspace/src/menu/notes/AddNoteBtn.java
+++ Workspace/src/menu/notes/AddNoteBtn.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 
 import menu.buffer.BufferPanel;
 
@@ -39,8 +41,23 @@ public class AddNoteBtn extends JButton
 			public void actionPerformed(ActionEvent arg0) 
 			{
 				System.out.println("Add Button");
-				
+				createInputWindow();
 			}
 		});
+	}
+	
+	public void createInputWindow()
+	{
+		JOptionPane optionPane = new JOptionPane("Enter Note Title:"
+                , JOptionPane.QUESTION_MESSAGE
+                , JOptionPane.CANCEL_OPTION
+                , null, null, "");
+		
+		
+		
+		optionPane.setWantsInput(true);             
+		JDialog dialog = optionPane.createDialog(null, "Create Note Name");
+		dialog.setLocation(10, 20);
+		dialog.setVisible(true);
 	}
 }
