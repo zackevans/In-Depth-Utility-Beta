ca644cd3dc1f9eef3a22b5f0e58b127a7508521b
Zack Evans <zackalta15@gmail.com>
2015-10-18 00:00:46 -0400

    added data models

---
+++ Workspace/src/menu/notes/NotesListData.java
@@ -0,0 +1,33 @@
+package menu.notes;
+
+import java.util.ArrayList;
+
+import sql.notes.NotesDataBase;
+
+public class NotesListData 
+{
+	NotesDataBase notesdb = new NotesDataBase();
+	private static ArrayList <String> sortedNames = new ArrayList <String>(); // names of data
+	
+	public void sortListNames()
+	{
+		sortedNames.clear();
+		
+		for (int i = 1; i <= notesdb.countItems(); i++)
+		{
+			String noteName = notesdb.getNoteNameFromPosition(i);
+			sortedNames.add(noteName);
+		}
+	}
+	
+	public ArrayList<String> getNoteListData()
+	{
+		sortListNames(); // puts names in correct sequence
+		return sortedNames;
+	}
+	
+	
+	
+	
+
+}
