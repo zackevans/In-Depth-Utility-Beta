9944d0193ff82af2d833f68856c0c5e0b927cd2a
Zack Evans <zackalta15@gmail.com>
2015-11-05 10:02:22 -0500

    added info button

---
+++ Workspace/src/menu/notes/InfoButton.java
@@ -0,0 +1,45 @@
+package menu.notes;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import menu.buffer.BufferPanel;
+
+public class InfoButton extends JButton
+{
+	private BufferPanel bufferPanel;
+	
+	public InfoButton (BufferPanel bufferPanel)
+	{
+		super();
+		this.bufferPanel = bufferPanel;
+	}
+	
+	public void initialize()
+	{
+		createBtn();
+		addListeners();
+	}
+	
+	public void createBtn()
+	{
+		setIcon(new ImageIcon("Images/Button_Images/Notes/Info.png"));
+		validate();
+	}
+
+	
+	public void addListeners()
+	{
+		addActionListener(new ActionListener() 
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				System.out.println("InfoBtn");
+			}
+		});
+	}
+}
