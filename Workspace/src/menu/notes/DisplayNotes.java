ea2e87880dd0710af1168e6147e60ae91cc14337
Zack Evans <zackalta15@gmail.com>
2015-11-05 09:38:55 -0500

    added display note component

---
+++ Workspace/src/menu/notes/DisplayNotes.java
@@ -0,0 +1,90 @@
+package menu.notes;
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import menu.buffer.BufferPanel;
+import sql.notes.NotesDataBase;
+
+public class DisplayNotes extends JScrollPane
+{
+	BufferPanel bufferPanel;
+	NotesDataBase notesdb = new NotesDataBase();
+	NotesList notesList = new NotesList(bufferPanel);
+	private static JTextArea textArea;
+	
+	
+	public DisplayNotes(BufferPanel bufferPanel)
+	{
+		super();
+		this.bufferPanel = bufferPanel;
+	}
+	
+	
+	public void initialize()
+	{
+		createComponents();
+		documentChange();
+	}
+	
+	public void createComponents()
+	{
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		setViewportView(textArea);
+	}
+	
+	public void displayNote()
+	{
+		
+		
+		
+		int id = notesList.getDBLocation();
+		
+		System.out.println("ID: " + id);
+	
+		if (id != -1)
+		{
+			String noteText = notesdb.getNotesBody(id);
+			textArea.setText(noteText);
+		}
+	}
+	
+	
+	public void documentChange()
+	{
+		textArea.getDocument().addDocumentListener(new DocumentListener()
+	    {
+	
+			public void changedUpdate(DocumentEvent arg0) 
+			{
+	        	   
+			}
+			public void insertUpdate(DocumentEvent arg0) 
+			{
+				updateChange();
+				
+			}
+	
+			public void removeUpdate(DocumentEvent arg0) 
+			{
+				updateChange();
+			}
+	    });
+	}
+	
+	public void updateChange()
+	{
+			int id = notesList.getDBLocation();
+			String currentText = textArea.getText();
+			notesdb.updateNotesBody(id, currentText);
+	 }
+
+}
