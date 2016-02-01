b284163a788c2c2ed6c135a8e483e045ec34cc56
Zack Evans <zackalta15@gmail.com>
2016-01-31 22:04:44 -0500

    added load data methods | added logic in action listener |  added getLastID() method | added setLastID() method

--- Workspace/src/menu/notes/NotesList.java
+++ Workspace/src/menu/notes/NotesList.java
@@ -2,6 +2,7 @@ package menu.notes;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -10,27 +11,25 @@ import javax.swing.JList;
 import javax.swing.JScrollPane;
 import sql.notes.NotesDataBase;
 
-public class NotesList extends JScrollPane
+public class NotesList
 {
 	public static final int Window_Width = 700;
 	public static final int Window_Height = 500;
-	//private static ArrayList <String> noteNames = new ArrayList <String>(); // names of data
-	private static JList list = new JList();
 	private NotesListData notesData = new NotesListData();
 	private NotesDataBase notesdb = new NotesDataBase();
-	private NotesListData notesListData = new NotesListData();
 	private Notes notes;
 	private SearchBar searchBar;
-	private DisplayNotes dispNotes; 
+	private DisplayNotes dispNotes;
+	public static JScrollPane scrollPane = new JScrollPane();
+	public static JList list = new JList();
 	public static int lastID = -1;
 	
-	public static int lastIndex = -1;
 	
 	public NotesList (Notes notes)
 	{
 		super();
 		this.notes = notes;
-		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		scrollPane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	}
 	
 	public void initialize()
@@ -42,7 +41,7 @@ public class NotesList extends JScrollPane
 	
 	public void createComponents()
 	{
-		setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		list = new JList(); // add data to the list here
 		list.setFont(new Font("Helvetica Neue",Font.PLAIN,17));
 		dispNotes = new DisplayNotes(notes);
@@ -51,20 +50,23 @@ public class NotesList extends JScrollPane
 	
 	public void layoutComponents()
 	{
-		setViewportView(list);	
+		scrollPane.setViewportView(list);	
 	}
 	
-	public void loadRawData()
+	@SuppressWarnings("unchecked")
+	public void loadRawData() // loads the raw data structure with what ever in it
 	{
 		list.setListData(notesData.getRawNoteListData().toArray());
 	}
 	
-	public void loadData()
+	@SuppressWarnings("unchecked")
+	public void loadData() // loads standard data from db 
 	{
 		list.setListData(notesData.getNoteListData().toArray());
 	}
 	
-	public void loadSearchData(String searchText)
+	@SuppressWarnings("unchecked")
+	public void loadSearchData(String searchText) //Sets list data = to the search data
 	{
 		list.setListData(notesData.getSearcNoteListData(searchText).toArray());
 	}
@@ -90,10 +92,14 @@ public class NotesList extends JScrollPane
 	        		if (searchBar.doesTextExist() == true)
 	        		{
 	        			int listIndex = list.getSelectedIndex();
+	        			int listPosition = notesData.getOldEquivalentPosition(listIndex);
+	        			
+	        			lastID = notesdb.getID(listPosition);
 	        			
 	        			notesData.moveListItemUp(listIndex);
 	        			loadRawData();
 	        			setListSeclection(0);
+	        			dispNotes.displayNote(lastID);
 	        		}
 	        		
 	        		else
@@ -120,111 +126,13 @@ public class NotesList extends JScrollPane
 		list.setSelectedIndex(listNumber);
 	}
 	
-	public int getDBLocation()
+	public int getLastID()
 	{
 		return lastID; // returns ID of last note clicked
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-//	public void addListeners()
-//	{
-//		list.addMouseListener(new MouseListener() 
-//		{
-//	        @Override
-//	        public void mouseReleased(MouseEvent e) {}
-//	        @Override
-//	        public void mousePressed(MouseEvent e) {}
-//	        @Override
-//	        public void mouseExited(MouseEvent e) {}
-//	        @Override
-//	        public void mouseEntered(MouseEvent e) {}
-//	        
-//	        @Override
-//	        public void mouseClicked(MouseEvent e) 
-//	        {  
-//	        	if (e.getClickCount() == 1)
-//	        	{	
-//	        		if (searchBar.doesTextExist() == true)
-//	        		{
-//	        			int listIndex = list.getSelectedIndex(); // starts at 0
-//	        			
-//	        			
-//	        			
-//	        			
-//	        			
-//	        		}
-//	        		
-//	        		else
-//	        		{
-//	        			int listIndex = list.getSelectedIndex();
-//		        		int listPosition = listIndex+1; 
-//		        		
-//		        		// created var for the ID(in db) of item clicked on
-//		        		int ID = notesdb.getID(listPosition); 
-//		        		// updates the item clicked on set to the first list position 
-//		        		
-//		        		notesdb.pushItemsAboveClickedDown(listPosition); // push items above clicked down
-//		        		notesdb.updateListPosition(ID, 1); // set item clicked to first
-//		        		
-//		        		lastIndex = 0;// when item is clicked it is moved to the first place
-//	        			
-//	        			updateListData();
-//		        		keepSelection();
-//	        		}
-//	        		
-//	        		dispNotes.displayNote();
-//	        		repaint();
-//	        	}
-//	        }
-//	    });
-//	}
-	
-//	public void updateListData()
-//	{
-//		noteNames.clear();
-//		noteNames = notesData.getNoteListData();
-//		list.setListData(noteNames.toArray());
-//	}
-//	
-//	public void updateSearchListData(String searchText)
-//	{
-//		noteNames.clear();
-//		noteNames = notesData.getSearcNoteListData(searchText);
-//		list.setListData(noteNames.toArray());
-//	}
-//	
-//	public void setListSelection(int listNumber)
-//	{
-//		list.setSelectedIndex(listNumber);
-//	}
-//	
-//	public void keepSelection()
-//	{
-//		if (lastIndex != -1)
-//		{
-//			list.setSelectedIndex(lastIndex);
-//		}
-//	}
-//	
-//	public void clearSelections()
-//	{
-//		list.clearSelection();
-//		lastIndex = -1;
-//	}
-//	
-//	public int getLastIndex ()
-//	{
-//		return lastIndex;
-//	}
-//	
-//	public int getDBLocation()
-//	{
-//		return notesdb.getID(lastIndex+1);
-//	}
+	public void setLastID(int lastID)
+	{
+		this.lastID = lastID;
+	}
 }
