1939281961fe7f033081fcd8c17da86f1b54865a
Zack Evans <zackalta15@gmail.com>
2016-03-02 11:43:54 -0500

    created new runnable for sending mail

--- Workspace/src/menu/notes/mailNotePanel/SendButton.java
+++ Workspace/src/menu/notes/mailNotePanel/SendButton.java
@@ -13,9 +13,11 @@ public class SendButton extends JButton
 {
 	BufferPanel bufferPanel;
 	private To to = new To();
+	private From from = new From();
 	private SeclectNote seclectNote = new SeclectNote();
 	private SendMail mail = new SendMail();
 	private NotesDataBase notesdb = new NotesDataBase();
+	private AdditionalComments additionalNotes = new AdditionalComments();
 
 	public SendButton (BufferPanel bufferPanel)
 	{
@@ -44,20 +46,43 @@ public class SendButton extends JButton
 				System.out.println("Send button");
 				int id = seclectNote.getLastID();
 				
-				System.out.println("ID = " + id);
-				
 				if (id != -1)
 				{
-					String s = To.textField.getText();
-					String[] to = {s};
-				
+					String[] to = {To.textField.getText()};
+					String fromField = From.textField.getText();
 					String subject = seclectNote.getNoteName();
 					String body = notesdb.getNotesBody(id);
+					String additionalComments = additionalNotes.textArea.getText();
+					
+					String finalBody = body + "\n\n" + additionalComments + "\n\n" + "-" + fromField;
 					
 					bufferPanel.showPanel("NOTES");
-					mail.sendNoteEMail(to, subject, body);
+					
+					Thread sendMail = new Thread(new PushEmail(to, subject, finalBody)); // create new thred to send email	
+					sendMail.start(); // start execution of the thred
 				}
 			}
 		});
+	}
+}
+
+class PushEmail implements Runnable 
+{
+	private SendMail mail = new SendMail();
+	String[] to;
+	String subject; 
+	String body;
+	
+	public PushEmail(String[] to, String subject, String body)
+	{
+		this.to = to;
+		this.subject = subject;
+		this.body = body;
+	}
+	
+	@Override
+	public void run() 
+	{
+		mail.sendNoteEMail(to, subject, body);
 	}
 }
