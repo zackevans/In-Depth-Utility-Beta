3dc2daa3b01f48897beebae2f5ca5e428b979896
Zack Evans <zackalta15@gmail.com>
2016-03-30 03:10:22 -0400

    finished logic

--- Workspace/src/menu/notes/notemailerrordialog/SaveAndSendButton.java
+++ Workspace/src/menu/notes/notemailerrordialog/SaveAndSendButton.java
@@ -8,13 +8,15 @@ import javax.swing.JButton;
 import menu.buffer.BufferPanel;
 import menu.notes.Notes;
 import sql.saveandsend.SaveAndSendDataBase;
+import sql.saveandsend.SaveAndSendSettingsDataBase;
 
 public class SaveAndSendButton extends JButton
 {
 	BufferPanel bufferPanel;
 	Notes notes = new Notes(bufferPanel);
 	private MailNoteErrorDialog mailNoteErrorDialog = new MailNoteErrorDialog(bufferPanel);
-	SaveAndSendDataBase saveAndSendDatabase = new SaveAndSendDataBase();
+	private SaveAndSendDataBase saveAndSendDatabase = new SaveAndSendDataBase();
+	private SaveAndSendSettingsDataBase saveAndSendSettingsDataBase = new SaveAndSendSettingsDataBase();
 	
 	public SaveAndSendButton(BufferPanel bufferPanel)
 	{
@@ -44,16 +46,13 @@ public class SaveAndSendButton extends JButton
 				
 				if(mailNoteErrorDialog.checkBox.isSelected())
 				{
-					
+					saveAndSendSettingsDataBase.updateNeverShow(true);
 				}
 				
-				else
-				{	
-					saveAndSendDatabase.createSavedEmail(MailNoteErrorDialog.to, MailNoteErrorDialog.subject ,MailNoteErrorDialog.body);
-					mailNoteErrorDialog.errorFrame.setVisible(false);
-					notes.infoButton.requestFocusInWindow();
-					bufferPanel.showPanel("NOTES");
-				}
+				saveAndSendDatabase.createSavedEmail(MailNoteErrorDialog.to, MailNoteErrorDialog.subject ,MailNoteErrorDialog.body);
+				mailNoteErrorDialog.errorFrame.setVisible(false);
+				notes.infoButton.requestFocusInWindow();
+				bufferPanel.showPanel("NOTES");
 			}
 		});	
 	}
