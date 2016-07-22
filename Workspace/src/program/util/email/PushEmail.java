a984db2bc9f362c2e08be2224e7a667b267fd17d
Zack Evans <zackalta15@gmail.com>
2016-07-22 11:47:00 -0400

    update utilities to send email

--- Workspace/src/program/util/email/PushEmail.java
+++ Workspace/src/program/util/email/PushEmail.java
@@ -1,14 +1,11 @@
 package program.util.email;
 
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+/**
+ * Class: PushEmail
+ * @author ZackEvans
+ *
+ * This class is a runnable thread that will send multiple emails in the background of the program.
+ */
 
 public class PushEmail implements Runnable 
 {
@@ -16,68 +13,33 @@ public class PushEmail implements Runnable
 	String subject;
 	String body;
 	
-	public PushEmail(String[] to, String subject, String body)
+	/**
+	 * Constructor: PushEmail(String[] to, String subject, String body) 
+	 * @author ZackEvans
+	 * @param to
+	 * @param subject
+	 * @param body
+	 * 
+	 * This constructor takes in an array of emails, subject, and a body of an email.
+	 */
+	
+	public PushEmail(String[] to, String subject, String body) 
 	{
 		this.to = to;
 		this.subject = subject;
 		this.body = body;
 	}
 	
+	/**
+	 * Function: run()
+	 * @author ZackEvans
+	 * 
+	 * This function overrides the defult runnable function to send an email.
+	 */
+	
 	@Override
 	public void run() 
 	{
-		sendNoteEMail(to, subject, body);
-	}
-	
-	public static void sendNoteEMail(String[] to, String subject, String body) 
-	{
-		String from = "idunoreply@gmail.com";
-		String pass = "swordfish15";
-		
-		Properties props = System.getProperties();
-	    String host = "smtp.gmail.com";
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.smtp.host", host);
-	    props.put("mail.smtp.user", from);
-	    props.put("mail.smtp.password", pass);
-	    props.put("mail.smtp.port", "587");
-	    props.put("mail.smtp.auth", "true");
-
-	    Session session = Session.getDefaultInstance(props);
-	    MimeMessage message = new MimeMessage(session);
-
-	    try 
-	    {
-	        message.setFrom(new InternetAddress(from));
-	        InternetAddress[] toAddress = new InternetAddress[to.length];
-
-	        // To get the array of addresses
-	        for( int i = 0; i < to.length; i++ ) 
-	        {
-	            toAddress[i] = new InternetAddress(to[i]);
-	        }
-
-	        for( int i = 0; i < toAddress.length; i++) 
-	        {
-	            message.addRecipient(Message.RecipientType.TO, toAddress[i]);
-	        }
-
-	        message.setSubject(subject);
-	        message.setText(body);
-	        Transport transport = session.getTransport("smtp");
-	        transport.connect(host, from, pass);
-	        
-	        transport.sendMessage(message, message.getAllRecipients());
-	        transport.close();
-	    }
-	    
-	    catch (AddressException ae) 
-	    {
-	        ae.printStackTrace();
-	    }
-	    catch (MessagingException me) 
-	    {
-	        me.printStackTrace();
-	    }
+		SendEmail.sendNoteEMail(to, subject, body); // send email.
 	}
 }
