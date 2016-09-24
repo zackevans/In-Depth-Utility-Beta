d899e60201dc8f6e8179c369ece724b59cb38394
Zack Evans <zackalta15@gmail.com>
2016-09-24 09:24:11 -0400

    update text box message

--- Workspace/src/menu/settings/settingspanels/passwordandsecuritypanel/passwordbooleanpanel/lockaftertimeoutpanel/LockAfterTimeoutSettingspanel.java
+++ Workspace/src/menu/settings/settingspanels/passwordandsecuritypanel/passwordbooleanpanel/lockaftertimeoutpanel/LockAfterTimeoutSettingspanel.java
@@ -5,9 +5,11 @@ import java.awt.FlowLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
+import sql.systemsettings.securitysettings.SecuritySettingsDatabase;
+
 public class LockAfterTimeoutSettingspanel extends JPanel
 {
-	public static JCheckBox lockAfterTimeoutCheckbox = new JCheckBox("Require Password After");
+	public static JCheckBox lockAfterTimeoutCheckbox = new LockCheckbox();
 	TimeCombobox timeCombobox = new TimeCombobox();
 	
 	public LockAfterTimeoutSettingspanel()
@@ -40,6 +42,12 @@ public class LockAfterTimeoutSettingspanel extends JPanel
 	{
 		add(lockAfterTimeoutCheckbox);
 		add(TimeCombobox.timeCombobox);
+	}
+	
+	public static void updateCheckbox()
+	{
+		SecuritySettingsDatabase securitySettingsDatabase = new SecuritySettingsDatabase();
+		lockAfterTimeoutCheckbox.setSelected(securitySettingsDatabase.getRequirePasswordValue());
 	}
 }
 	
