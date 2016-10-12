de5a207810845e6846b53cc5005cf86796c19a63
Zack Evans <zackalta15@gmail.com>
2016-10-12 10:33:38 -0400

    update job handler

--- Workspace/src/jobs/handler/JobHandler.java
+++ Workspace/src/jobs/handler/JobHandler.java
@@ -1,16 +1,7 @@
 package jobs.handler;
 
-import org.quartz.CronScheduleBuilder;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
-import org.quartz.impl.StdSchedulerFactory;
-
-import jobs.jobs.CheckAndSendEmailJob;
-import jobs.jobs.UpdateTimeJob;
+import jobs.handler.email.CheckAndSendEmailJobScheduler;
+import jobs.handler.statusbar.UpdateTimeScheduler;
 
 public class JobHandler 
 {
@@ -22,76 +13,9 @@ public class JobHandler
 	
 	public void createJobs()
 	{
-		createUpdateTimeJob();
-		createCheckAndSendEmailJob();
+		UpdateTimeScheduler.createUpdateTimeJob();
+		CheckAndSendEmailJobScheduler.createCheckAndSendEmailJob();
 	}
 	
-	/**
-     * @author ZackEvans
-     * Function: createUpdateTimeJob()
-     * 
-     * every min run a job that updates the time
-     */
-    
-    public static void createUpdateTimeJob()
-    {
-    	JobDetail job = JobBuilder.newJob(UpdateTimeJob.class) // create a job detail from updatetimejob.java
-    			.withIdentity("UpdateTimeJob", "updateJobs").build();
-    	
-    	Trigger trigger = TriggerBuilder // create trigger
-    			.newTrigger()
-    			.withIdentity("UpdateTimeJob", "updateJobs")
-    			.withSchedule(CronScheduleBuilder.cronSchedule("0 0/1 * 1/1 * ? *")) // set trigger to run every min
-    			.build();
-    
-    	Scheduler scheduler; // create a schedule object
-    	
-		try 
-		{
-			// try to schedule the job
-			scheduler = new StdSchedulerFactory().getScheduler();
-			scheduler.start();
-	    	scheduler.scheduleJob(job, trigger);
-		} 
-		
-		catch (SchedulerException e) 
-		{
-			System.err.println("createUpdateTimeJob() - Unable to Create Email Job");
-			e.printStackTrace();
-		}
-    }
 	
-	/**
-     * Function: createCheckAndSendEmailJob()
-     * @author ZackEvans
-     * 
-     * Function runs every 30 min to check if emails can be sent.
-     */
-    
-    public static void createCheckAndSendEmailJob()
-    {
-    	JobDetail job = JobBuilder.newJob(CheckAndSendEmailJob.class) // create the job todo
-    			.withIdentity("CheckAndSendEmailJob", "emailJobs").build();
-    	
-    	Trigger trigger = TriggerBuilder // create a trigger for when it should happen
-    			.newTrigger()
-    			.withIdentity("CheckAndSendEmail", "emailTriggers")
-    			.withSchedule(CronScheduleBuilder.cronSchedule("0 0/30 * 1/1 * ? *"))
-    			.build();
-    
-    	Scheduler scheduler; // schedule the process
-    	
-		try 
-		{
-			scheduler = new StdSchedulerFactory().getScheduler();
-			scheduler.start();
-	    	scheduler.scheduleJob(job, trigger);
-		} 
-		
-		catch (SchedulerException e) 
-		{
-			System.out.println("createCheckAndSendEmailJob() - Unable to Create Email Job");
-			e.printStackTrace();
-		}
-    }
 }
---
+++ Workspace/src/jobs/handler/email/CheckAndSendEmailJobScheduler.java
@@ -0,0 +1,49 @@
+package jobs.handler.email;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+import jobs.jobs.CheckAndSendEmailJob;
+
+public class CheckAndSendEmailJobScheduler 
+{
+	/**
+     * Function: createCheckAndSendEmailJob()
+     * @author ZackEvans
+     * 
+     * Function runs every 30 min to check if emails can be sent.
+     */
+    
+    public static void createCheckAndSendEmailJob()
+    {
+    	JobDetail job = JobBuilder.newJob(CheckAndSendEmailJob.class) // create the job todo
+    			.withIdentity("CheckAndSendEmailJob", "emailJobs").build();
+    	
+    	Trigger trigger = TriggerBuilder // create a trigger for when it should happen
+    			.newTrigger()
+    			.withIdentity("CheckAndSendEmail", "emailTriggers")
+    			.withSchedule(CronScheduleBuilder.cronSchedule("0 0/30 * 1/1 * ? *"))
+    			.build();
+    
+    	Scheduler scheduler; // schedule the process
+    	
+		try 
+		{
+			scheduler = new StdSchedulerFactory().getScheduler();
+			scheduler.start();
+	    	scheduler.scheduleJob(job, trigger);
+		} 
+		
+		catch (SchedulerException e) 
+		{
+			System.out.println("createCheckAndSendEmailJob() - Unable to Create Email Job");
+			e.printStackTrace();
+		}
+    }
+}
