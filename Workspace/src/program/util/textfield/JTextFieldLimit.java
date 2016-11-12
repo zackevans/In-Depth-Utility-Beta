93e0d07984959779f7ff98247f393e83525ad1e8
Zack Evans <zackalta15@gmail.com>
2016-11-12 02:36:28 -0500

    add Util class

---
+++ Workspace/src/program/util/textfield/JTextFieldLimit.java
@@ -0,0 +1,42 @@
+package program.util.textfield;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+public class JTextFieldLimit extends PlainDocument 
+{
+	private int limit;
+	
+	public JTextFieldLimit(int limit) 
+	{
+		super();
+	    this.limit = limit;
+	}
+	
+	public JTextFieldLimit(int limit, boolean upper) 
+	{
+		super();
+	    this.limit = limit;
+	}
+	
+	
+	@Override
+	public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException 
+	{
+		if (str == null)
+	    {
+	    	return;
+	    }
+		
+		if ((getLength() + str.length()) <= limit) 
+		{
+			super.insertString(offset, str, attr);
+		}
+	}
+}
+	  
+	  
+
+	  
+
